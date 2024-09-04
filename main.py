3f083f785f2d2477f0d3caae6d19ab1f1bd34038
Celray James CHAWANDA <celray.chawanda@outlook.com>
2024-09-04 14:18:49 -0500

    update v2.0

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-from cjfx import *
+from cjfx import geopandas
 
+# functions
+def main():
+    # create a geopandas object
+    gdf = geopandas.GeoPandas()
+
+    # load the data
+    gdf.load_data()
+
+    # plot the data
+    gdf.plot_data()
 
